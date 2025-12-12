b4f977106796ddaf57238e3ddc99493e58f9352e
Ridvik Pal <ridvikpal@gmail.com>
2025-12-12 08:59:51 -0500

    Updated gsettings update script with echo messages to inform the user what is happening.

--- gsettings_update.sh
+++ gsettings_update.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+# This script changes some hidden gnome settings using gsettings
+# for my preferred user experience.
+
 # set the background to black
+echo "Setting background to black"
 gsettings set org.gnome.desktop.background picture-options 'none'
 gsettings set org.gnome.desktop.background primary-color '#000000'
+echo "Done setting the background to black"
+echo ""
 
 # disable touchpad and trackpoint acceleration
+echo "Setting touchpad and pointing stick acceleration to flat"
 gsettings set org.gnome.desktop.peripherals.touchpad accel-profile 'flat'
 gsettings set org.gnome.desktop.peripherals.pointingstick accel-profile 'flat'
+echo "Done setting touchpad and pointing stick acceleration to flat"
+echo ""
 
 # disable conflicting VS Code copy line up/down keyboard shortcuts
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down ['']
+echo "Disabling move-to-workspace-up/down keyboard shortcuts"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up ['']
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down ['']
+echo "Done disabling move-to-workspace-up/down keyboard shortcuts"
+echo ""
 
-# In case you ever want to reset a gsettings key-value pair, use:
-# gsettings reset SCHEMA [:PATH]  KEY
-# Ex: gsettings reset org.gnome.desktop.wm.keybindings move-to-workspace-down
+echo "In case you ever want to reset a gsettings key-value pair, use:"
+echo "gsettings reset SCHEMA [:PATH]  KEY"
+echo "Ex: gsettings reset org.gnome.desktop.wm.keybindings move-to-workspace-down"
